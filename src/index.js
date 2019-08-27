e2fdede1cf56d18e3a6ad6b55a3aebe6e816cd89
Mohammed AKLI <mohammedakli05@gmail.com>
2019-08-27 11:11:51 +0200

    login and sign up, deliver JWT token, delete user, list users

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('config');
+const passport = require('passport');
 const apiRouter = require('./services/api');
 const errors = require('./middleware/errors');
 const notFound = require('./middleware/notFound');
+
 
 const server = express();
 // Middlware Généraux
@@ -16,9 +18,12 @@ server.use(apiRouter);
 // Last one
 server.use(notFound);
 
+// passport Init
+
+server.use(passport.initialize());
+server.use(passport.session());
+
 // Middleware d'erreur
 server.use(errors);
 
-server.listen(config.get('port'), () => {
-  console.log(`Server started at port ${config.get('port')}`);
-});
+server.listen(3000);

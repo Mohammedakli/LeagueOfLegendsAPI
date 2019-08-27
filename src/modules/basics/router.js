2daf30b7c7ce12e9eeb89ab657e2fcf45a85a66e
Mohammed AKLI <mohammedakli05@gmail.com>
2019-08-27 11:08:34 +0200

    route configuration

--- src/modules/basics/router.js
+++ src/modules/basics/router.js
@@ -1,4 +1,15 @@
+// eslint-disable no-console //
 const { Router } = require('express');
+
+const passport = require('passport');
+
+const registerUser = require('../../modules/users/registerUser');
+
+const loginUser = require('../../modules/users/loginUser')
+
+const listUser = require('../../modules/users/listUsers')
+
+const deleteUser = require('../../modules/users/deleteUser')
 
 const router = new Router();
 
@@ -9,5 +20,53 @@ router.use('/ping', (req, res) => {
     ping: 'pong',
   });
 });
+
+
+router.post('/api/register', (req, res) => {
+  try {
+    registerUser.register(req, res, () => {
+      console.log('ok');
+    });
+  } catch (errr) {
+    console.log(errr);
+  }
+});
+
+router.post('/api/login', (req, res) => {
+  try {
+    loginUser.authenticate(req, res, () => {
+      console.log('ok');
+    });
+  } catch (errr) {
+    console.log(errr);
+  }
+  
+});
+
+router.get('/api/login', (req, res) => {
+  try {
+    listUser.show(req, res, () => {
+      console.log('ok');
+    });
+  } catch (errr) {
+    console.log(errr);
+  }
+  
+});
+
+router.delete('/api/login', (req, res) => {
+  try {
+    deleteUser.Delete(req, res, () => {
+      console.log('ok');
+    });
+  } catch (errr) {
+    console.log(errr);
+  }
+  
+});
+
+
+  
+
 
 module.exports = router;

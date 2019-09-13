642a441fa2dedee41f136942057730ea966b012f
Mohammed AKLI <mohammedakli05@gmail.com>
2019-09-13 16:18:57 +0200

    add mysql and other stuff

---
+++ config/passport.js
@@ -0,0 +1,13 @@
+const passport = require('passport');
+const User = require('../src/modules/users');
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
